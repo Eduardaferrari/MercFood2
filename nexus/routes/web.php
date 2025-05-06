1f096ca27e643290b05bc63ab69d4642d024f9d8
Cassiwizar <masterticole@gmail.com>
2025-05-06 17:05:34 -0300

    Foi

--- nexus/routes/web.php
+++ nexus/routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\TelaComandaController;
 use App\Http\Controllers\ClientesController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\reservaController;
+
 
 Route::get('/', function () {
     return view('login');
@@ -16,3 +18,4 @@ Route::get('/site', [SiteController::class, 'site'])->name('site');
 Route::get('/cadastro', [CadastroController::class, 'cadastro'])->name('cadastro');
 Route::get('/telaComanda', [TelaComandaController::class, 'telaComanda'])->name('telaComanda');
 Route::resource('clientes', ClientesController::class);
+Route::get('/reserva', [reservaController::class, 'reserva'])->name('reserva');
